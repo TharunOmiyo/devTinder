fc1ee178f54fd33b5368a35dbb10e8ec4fb34fba
Pullareddy Tharun <pullareddytharun8@gmail.com>
2025-02-11 20:36:08 +0530

    "Addidng multiple routes with next"

--- src/app.js
+++ src/app.js
@@ -6,34 +6,59 @@ const data = {
   firstName: "Tharun",
   lastName: "Pullareddy",
 };
-app.get("/", (req, res) => {
-  res.send("Hello from ");
-});
-app.post("/profile", (req, res) => {
-  console.log("Data is created");
-  res.send(data);
-});
-app.delete("/profile", (req, res) => {
-  console.log("Data is deleted");
-  res.send("Data is deleted");
-});
-app.patch("/profile", (req, res) => {
-  res.send("data updated ");
-});
-app.get("/test", (req, res) => {
-  res.send("Hello from test");
-});
-app.get("/profile", (req, res) => {
-  if (req.query) {
-    res.send(req.query);
+// app.get("/", (req, res) => {
+//   res.send("Hello from ");
+// });
+// app.post("/profile", (req, res) => {
+//   console.log("Data is created");
+//   res.send(data);
+// });
+// app.delete("/profile", (req, res) => {
+//   console.log("Data is deleted");
+//   res.send("Data is deleted");
+// });
+// app.patch("/profile", (req, res) => {
+//   res.send("data updated ");
+// });
+// app.get("/test", (req, res) => {
+//   res.send("Hello from test");
+// });
+// app.get("/profile", (req, res) => {
+//   if (req.query) {
+//     res.send(req.query);
+//   }
+//   res.send("Hello from profile");
+// });
+// app.get("/profile/:proId", (req, res) => {
+//   console.log(req.params);
+//   res.send(req.params);
+// });
+app.use(
+  "/users",
+  [
+    (req, res, next) => {
+      console.log("1st Response");
+      next();
+    },
+    (req, res, next) => {
+      console.log("2nd Response");
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log("3rd Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("5th Response");
+    // next();
+    res.send("Hi from user");
   }
-  res.send("Hello from profile");
-});
-app.get("/profile/:proId", (req, res) => {
-  console.log(req.params);
-  res.send(req.params);
-});
-
+);
 // app.use((req, res) => {
 //   res.send("Error page");
 // });
